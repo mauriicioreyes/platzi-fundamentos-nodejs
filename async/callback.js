bd69c8f28ff7ebc157df48bd630506f0adc32726
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-13 20:19:37 -0500

    Modifica función 'adios' para recibir el nombre directo de la función anterior

--- async/callback.js
+++ async/callback.js
@@ -1,7 +1,7 @@
 function hola(nombre, miCallback) {
     setTimeout(function () {
         console.log('Hola ' + nombre);
-        miCallback();
+        miCallback(nombre);
     }, 1500);
 }
 
@@ -13,11 +13,11 @@ function adios(nombre, otroCallback) {
 }
 
 console.log('Iniciando proceso ...');
-// hola('Carlos',function () {
-//     adios('Carlos', function () {
-//         console.log('Terminando proceso ...');
-//     });
-// });
+hola('Carlos',function (nombre) {
+    adios(nombre, function () {
+        console.log('Terminando proceso ...');
+    });
+});
 
-hola('Carlos', function () {});
-adios('Carlos', function () {});
+// hola('Carlos', function () {});
+// adios('Carlos', function () {});
