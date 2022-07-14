f09bdec01e1cf7d0acf741751cf7d3b3b4919e40
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-14 00:27:54 -0500

    Creamos una función para escribir archivos

--- modulos/fs.js
+++ modulos/fs.js
@@ -6,4 +6,16 @@ function leer(ruta, cb) {
     })
 }
 
+function escribir(ruta, contenido, cb) {
+    fs.writeFile(ruta, contenido, function (err) {
+        if (err) {
+            console.error('No se ha podido escribir', err);
+        } else {
+            console.log('Se ha escrito el archivo correctamente');
+        }
+    });
+}
+
 leer(__dirname + '/archivo.txt', console.log);
+
+escribir(__dirname + '/archivo_escrito.txt', 'Soy un archivo nuevo creado desde código.', console.log);
