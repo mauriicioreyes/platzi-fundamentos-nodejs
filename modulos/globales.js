3afa6cdcd6181b2ab1d4a3de59000e22fb88a00e
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-13 23:20:30 -0500

    Implementando función global 'setImmediate'

--- modulos/globales.js
+++ modulos/globales.js
@@ -1,10 +1,3 @@
-console.log(setInterval);
-
-let i = 0;
-let intervalo = setInterval(function () {
+setImmediate(function () {
     console.log('Hola');
-    if (i === 3){ 
-        clearInterval(intervalo);
-    }
-    i++;
-}, 1000);
+})
