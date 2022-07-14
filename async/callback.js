7b101f8cc4a7128931b64562d41f36c2de13db7b
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-13 20:04:21 -0500

    Agrega a setTimeout al archivo para implementar asincronísmo

--- async/callback.js
+++ async/callback.js
@@ -1,5 +1,8 @@
 function soyAsincrona() {
     console.log('Hola, soy una función asíncrona');
+    setTimeout(function () {
+        console.log('Estoy siendo asíncrona');
+    }, 1000);
 }
 
 console.log('Iniciando proceso ...')
