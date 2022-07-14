2dc37fe6497944745af7e91339f936e7646e641a
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-13 19:26:26 -0500

    Agrega validación a las variables de entorno y respuesta si no recibe la variable

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -1,3 +1,5 @@
-let nombre = process.env.NOMBRE;
+let nombre = process.env.NOMBRE || 'DESCONOCIDO :/';
+let web = process.env.WEB || 'N/A';
 
 console.log('Hola ' + nombre);
+console.log('Tu página web es ' + web);
