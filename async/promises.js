930890658d9d1de59fc587e0c9482fc02ea2a1c8
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-13 21:54:27 -0500

    Convirtiendo funciones con callbacks a promesas de JS

--- async/promises.js
+++ async/promises.js
@@ -1,20 +1,39 @@
-function hola(nombre, miCallback) {
-    setTimeout(function () {
-        console.log('Hola ' + nombre);
-        miCallback(nombre);
-    }, 1500);
+function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('Hola ' + nombre);
+            resolve(nombre);
+        }, 1500);
+    });
 }
 
-function adios(nombre, otroCallback) {
-    setTimeout(function () {
-        console.log('Adios ' + nombre);
-        otroCallback();
-    }, 1000);
+function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Adios ' + nombre);
+            resolve();
+        }, 1000);
+    });
 }
 
-function hablar(callbackHablar) {
-    setTimeout(function () {
-        console.log('Bla bla bla bla...');
-        callbackHablar();
-    }, 1000);
+function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Bla bla bla bla...');
+            resolve(nombre);
+        }, 1000);
+    });
 }
+
+// ---
+
+console.log('Iniciando el proceso...');
+hola('Daniel')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminado el proceso');
+    })
