0e28c6ada369b12f915350143787ebcd63bbe704
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 14:28:12 -0500

    Agrega método 'console.count' para crear un contador directo en consola.

--- modulos/consola.js
+++ modulos/consola.js
@@ -1,18 +1,14 @@
-function func1() {
-    console.group('Función 1');
-    console.log('esto es de la función 1')
-    console.log('esto también es de la función 1')
-    func2();
-    console.log('Volvemos a la función 1');
-    console.groupEnd('Funcion 1');
-}
-
-function func2() {
-    console.group('Funcion 2');
-    console.log('estamos en la función 2');
-    console.groupEnd('Funcion 2');
-}
-
-console.log('Empezamos');
-func1();
-console.log('Terminamos');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
