697f7279b357c266b4b944360f5a315fa859beda
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 15:49:49 -0500

    Agregamos manejo de errores mediante Try / Catch en función asíncrona.

--- modulos/errores.js
+++ modulos/errores.js
@@ -6,15 +6,22 @@ function seRompe() {
     return 3 + z;
 }
 
-function seRompeAsincrona() {
+function seRompeAsincrona(callback) {
     setTimeout(function () {
-        return 10 + x;
+        try {
+            return 10 + x;
+        } catch (err) {
+            console.error('Error en función asíncrona...', err.message);
+            callback(err);
+        }
     }, 1000);
 }
 
 try {
     // otraFuncion();
-    seRompeAsincrona();
+    seRompeAsincrona(function (err) {
+        console.log(err.message);
+    });
 } catch(err) {
     console.error('Vaya, algo se ha roto... ', err.message);
     console.error(err);
