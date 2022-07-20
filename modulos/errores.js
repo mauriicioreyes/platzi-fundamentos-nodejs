1fbc4d7764e0cb738dac9875463b1324e851e1db
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 15:42:42 -0500

    Agregamos una función asíncrona que truena para probar que sucede

--- modulos/errores.js
+++ modulos/errores.js
@@ -6,8 +6,15 @@ function seRompe() {
     return 3 + z;
 }
 
+function seRompeAsincrona() {
+    setTimeout(function () {
+        return 10 + x;
+    }, 1000);
+}
+
 try {
-    otraFuncion();
+    // otraFuncion();
+    seRompeAsincrona();
 } catch(err) {
     console.error('Vaya, algo se ha roto... ', err.message);
     console.error(err);
