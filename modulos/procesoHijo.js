f32e44cb2f7aace05453652d7915d5efb8493b64
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 16:11:39 -0500

    Modifica comando ejecutado, para comprobar que exec ejecuta comandos 'anidados'.

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 // const exec = require('child_process').exec;
 
-exec('ls -lah', (err, stdout, sterr) => {
+exec('node modulos/consola.js', (err, stdout, sterr) => {
     if (err) {
         console.error('Error:', err.message);
         return false;
