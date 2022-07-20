75878f47d14fcd1acd5981f84658055e962cb620
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 14:18:42 -0500

    Agrega método 'console.group' para agrupar resultados de consola.

--- modulos/consola.js
+++ modulos/consola.js
@@ -1,13 +1,9 @@
-var tabla = [
-    {
-        a: 1,
-        b: 'Z'
-    },
-    {
-        a: 2,
-        b: 'A'
-    }
-]
-
-console.log(tabla);
-console.table(tabla);
+console.group('inicio');
+console.log('Hola');
+console.log('¿Cómo estás?');
+console.group('dialogo');
+console.log('Bla bla bla');
+console.log('Bla bla bla bla bla');
+console.groupEnd('dialogo');
+console.log('Adios');
+console.groupEnd('conv');
