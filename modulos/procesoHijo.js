063fc32b95b5b745ab86dde2fcdf688f5f68f92b
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 17:21:27 -0500

    Implementando 'spawn' para manejo de procesos hijo.
    Spawn nos ayuda a obtener el estado del proceso hijo y manejarlo más a detalle.

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,11 +1,25 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 // const exec = require('child_process').exec;
 
-exec('node modulos/consola.js', (err, stdout, sterr) => {
-    if (err) {
-        console.error('Error:', err.message);
-        return false;
-    }
+// exec('node modulos/consola.js', (err, stdout, sterr) => {
+//     if (err) {
+//         console.error('Error:', err.message);
+//         return false;
+//     }
 
-    console.log(stdout);
+//     console.log(stdout);
+// });
+
+let proceso = spawn('ls', ['-lah']);
+console.log('PID del proceso', proceso.pid);
+console.log('Proceso conectado', proceso.connected);
+
+proceso.stdout.on('data', function (dato) {
+    console.log('Proceso muerto', proceso.killed);
+    console.log(dato.toString());
 });
+
+proceso.on('exit', function () {
+    console.log('El proceso terminó');
+    console.log('Proceso muerto', proceso.killed);
+})
