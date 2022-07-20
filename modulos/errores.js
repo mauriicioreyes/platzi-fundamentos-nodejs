b74b948346810572f2a3a48b5e7869358acc92ce
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 15:32:29 -0500

    Agregamos manejo de errores mediante Try / Catch

--- modulos/errores.js
+++ modulos/errores.js
@@ -2,4 +2,11 @@ function seRompe() {
     return 3 + z;
 }
 
-seRompe();
+try {
+    seRompe();
+} catch(err) {
+    console.error('Vaya, algo se ha roto... ', err.message);
+    console.error(err.message);
+}
+
+console.log('Elemento al final');
