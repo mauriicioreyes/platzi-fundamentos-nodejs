fcd5d3988b6a0692eaf2b0deccddca2aa9c96a7a
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 15:37:15 -0500

    Traza de errores dentro de otras funciones

--- modulos/errores.js
+++ modulos/errores.js
@@ -1,12 +1,16 @@
+function otraFuncion() {
+    seRompe();
+}
+
 function seRompe() {
     return 3 + z;
 }
 
 try {
-    seRompe();
+    otraFuncion();
 } catch(err) {
     console.error('Vaya, algo se ha roto... ', err.message);
-    console.error(err.message);
+    console.error(err);
 }
 
 console.log('Elemento al final');
