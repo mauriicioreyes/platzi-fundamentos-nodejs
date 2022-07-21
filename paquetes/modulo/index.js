62ae5287504052a87cd93d1248cfecd5a6419dd5
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-21 13:12:57 -0500

    Agrega 'modulo.js' y 'index.js' para crear módulos y aprender a exportarlos.
    Forma más básica de crear un módulo en NodeJS.

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,8 @@
+// Traer nuestro módulo
+const modulo = require('./modulo');
+
+// console.log(modulo);
+modulo();
+
+
+// Ejecutar una función del módulo
---
+++ paquetes/modulo/modulo.js
@@ -0,0 +1,5 @@
+function saludar() {
+    console.log('Hola a todos :D');
+}
+
+module.exports = saludar;
