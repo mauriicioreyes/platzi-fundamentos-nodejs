c0e6c5431cc2d619b2017e34d99482268c8714fe
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-20 19:44:47 -0500

    Crea una función llamada 'Router', responde a '/hola', y el resto muestra 'Error 404'.

--- modulos/http.js
+++ modulos/http.js
@@ -1,14 +1,25 @@
 const http = require('http');
 
-http.createServer(function (request, response) {
+http.createServer(router).listen(3000);
+
+function router(request, response) {
     console.log('Nueva petición!');
     console.log(request.url);
 
+    switch (request.url) {
+        case '/hola':
+            response.write('Hola, que tal');
+            response.end();
+            break;
+        default:
+            response.write('Error 404: Elemento no encontrado :(');
+            response.end();
+    }
     // Respuesta al usuario
-    response.writeHead(201, { 'Content-Type': 'text/plane'});
-    response.write('Hola, ya se usar HTTP de NodeJS');
+    // response.writeHead(201, { 'Content-Type': 'text/plane' });
+    // response.write('Hola, ya se usar HTTP de NodeJS');
 
-    response.end();
-}).listen(3000);
+    // response.end();
+}
 
 console.log('Escuchando http en el puerto 3000');
