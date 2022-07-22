d6c67fc996978eec16294411f76bf8f4cfa40c79
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-21 22:18:19 -0500

    Crea Stream de lectura y escritura, dirigiendo su salida al Stdout del sistema.

--- memoria/stream.js
+++ memoria/stream.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 
+const stream = require('stream');
+
+const util = require('util');
+
 let data = '';
 
 let readableStream = fs.createReadStream(__dirname + '/input.txt');
 
-readableStream.setEncoding('utf8');
+// readableStream.setEncoding('utf8');
 
 // readableStream.on('data', function (chunk) {
 //     data += chunk;
@@ -15,7 +19,27 @@ readableStream.setEncoding('utf8');
 // });
 
 // Escribiendo en el buffer de la salida estándar del sistema.
-process.stdout.write('Hola');
-process.stdout.write('que');
-process.stdout.write('tal');
-process.stdout.write(':D');
+// process.stdout.write('Hola');
+// process.stdout.write('que');
+// process.stdout.write('tal');
+// process.stdout.write(':D');
+
+const Transform = stream.Transform;
+
+function Mayus() {
+    Transform.call(this);
+}
+
+util.inherits(Mayus, Transform);
+
+Mayus.prototype._transform = function(chuck, codif, cb) {
+    chuckMayus = chuck.toString().toUpperCase();
+    this.push(chuckMayus);
+    cb();
+}
+
+let mayus = new Mayus();
+
+readableStream
+    .pipe(mayus)
+    .pipe(process.stdout);
