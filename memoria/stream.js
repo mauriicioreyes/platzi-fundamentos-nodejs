703f549ada851aecedc8b8acf3c83ecee61f6170
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-21 21:22:24 -0500

    Crea buffer de escritura en la salida estándar del sistema.

--- memoria/stream.js
+++ memoria/stream.js
@@ -6,10 +6,16 @@ let readableStream = fs.createReadStream(__dirname + '/input.txt');
 
 readableStream.setEncoding('utf8');
 
-readableStream.on('data', function (chunk) {
-    data += chunk;
-});
+// readableStream.on('data', function (chunk) {
+//     data += chunk;
+// });
 
-readableStream.on('end', function () {
-    console.log(data);
-})
+// readableStream.on('end', function () {
+//     console.log(data);
+// });
+
+// Escribiendo en el buffer de la salida estándar del sistema.
+process.stdout.write('Hola');
+process.stdout.write('que');
+process.stdout.write('tal');
+process.stdout.write(':D');
