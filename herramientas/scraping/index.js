b3d63c0355c4757e9d142d284f97d2db81f18cec
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-27 14:54:47 -0500

    Crea una variable que evalua la wiki de Node.js
    Trae el elemento 'h1' de la página y lo muestra en consola.
    Retorna el elemento y lo muestra en nuestra terminal.

--- herramientas/scraping/index.js
+++ herramientas/scraping/index.js
@@ -9,6 +9,13 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://es.wikipedia.org/wiki/Node.js');
 
+    var titulo1 = await page.evaluate(() => {
+        const h1 = document.querySelector('h1');
+        console.log(h1.innerHTML);
+
+        return h1.innerHTML;
+    });
+    console.log('h1:', titulo1);
     console.log('Cerramos navegador...');
     browser.close();
     console.log('Navegador cerrado.');
