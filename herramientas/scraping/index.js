7feabfd41970ca4983993e0071da206541cb2a03
Mauricio Reyes (wsl) <mauricioreyes.dev@gmail.com>
2022-07-27 14:08:35 -0500

    Implementamos métodos de puppeteer para abrir un navegador y acceder a la wiki de Node.js

--- herramientas/scraping/index.js
+++ herramientas/scraping/index.js
@@ -3,4 +3,13 @@ const puppeteer = require('puppeteer');
 // Función anónima autoejecutada, convertida en expresión
 (async () => {
     // código
+    console.log('Lanzamos navegador!');
+    // const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://es.wikipedia.org/wiki/Node.js');
+
+    console.log('Cerramos navegador...');
+    browser.close();
+    console.log('Navegador cerrado.');
 })();
